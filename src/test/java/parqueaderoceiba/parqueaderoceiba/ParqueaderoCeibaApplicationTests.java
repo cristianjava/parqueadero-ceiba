bfd457d01f8bb6d3f20952fca32893f072cf57ad
Cristian David Castaño Giraldo - Ceiba Software <cristian.castano@ceiba.com.co>
2018-07-30 14:05:17 -0500

    Se suben cambios de la estructura inicial con gradle

---
+++ src/test/java/parqueaderoceiba/parqueaderoceiba/ParqueaderoCeibaApplicationTests.java
@@ -0,0 +1,16 @@
+package parqueaderoceiba.parqueaderoceiba;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParqueaderoCeibaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
